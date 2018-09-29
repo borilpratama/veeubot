c794c2ea70dc092b9a01cc80944c186512153882
borilpratama <borilpratama675@gmail.com>
2018-09-29 21:47:48 +0000

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 //masukan config token yg terdapat pada link di packet captur
 // jangan hapus tanda kutip(" ") 
 
-$access_token = "c1b6043c-d6e6-43f5-96e8-d118a105db27";
+$access_token = "0fdb3105-4f55-4917-bdb5-2d4b6ff6cc18";
 
 // kamu bisa membuat banyak config untuk beberapa akun.
